304a61ec0a20f079b0554532743b2dea4f3004c3
nicholsonES_EDIT <nicholson@empirestate.co.za>
2015-08-21 10:00:32 +0200

    Responsive on the chat page
    fixing responsive for all device sizes

--- chat.php
+++ chat.php
@@ -83,7 +83,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <div class="panel panel-default">
+          <div class="panel panel-default fixed-height">
             <!-- Add class "collapsed" to minimize the panel -->
             <div class="panel-body">
               <div class="tab-content">
